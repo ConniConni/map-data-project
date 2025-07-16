bee7c2d5b43db9c658c9d36672ee39d8b9253ada
ConniConni <nmtgktmo@gmail.com>
2025-07-16 16:41:00 +0900

    練習: クエリ実行と実行結果をCSVに保存

--- main2.py
+++ main2.py
@@ -1,4 +1,7 @@
 import psycopg2
+import psycopg2.extras
+from itertools import islice
+import csv
 
 
 # DB接続情報を定義
@@ -10,6 +13,20 @@ DB_CONFIG = {
     "password": "postgres",
 }
 
+# 東京都に絞り、id,都道府県,市区町村名,管理コード,エリアテキストを表示するSQL文
+SQL_QUERY = """
+SELECT
+    id,
+	prefecture,
+	city_name,
+	admin_code,
+	ST_AsText(geom) AS area
+FROM
+    V_JAPAN_ADMIN
+WHERE
+    prefecture = '岩手県';
+"""
+
 
 def main():
     conn = None  # connを初期化
@@ -19,11 +36,41 @@ def main():
         print("#### DBに接続しました。 ####")
 
         # カーソルを作成
-        cur = conn.cursor()
-        print("#### カーソルを作成しました。 ####")
+        with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cur:
+            print("#### カーソルを作成しました。 ####")
 
-        # カーソルを削除
-        cur.close()
+            # SQLを実行
+            cur.execute(SQL_QUERY)
+
+            # 実行結果をまとめて取得する
+            rows = cur.fetchall()
+
+            # 実行結果がない場合、コンソールへの表示をスキップしてDB接続を閉じる
+            if not rows:
+                print("データが見つかりませんでした。")
+                return
+
+            # コンソールに実行結果を表示（結果は１件のみ表示）
+            print("=== 実行結果表示 ===")
+            for row in islice(rows, 1):
+                print(f"市区町村名: {row['city_name']},面積: {row['area']}")
+
+            # 結果をcsvに書き込み（30件のみ）
+            try:
+                with open("iwate_areas.csv", "w", encoding="utf-8") as f:
+                    header = rows[0].keys()
+                    writer = csv.writer(f)
+                    writer.writerow(header)
+
+                    # 各行のデータを書き込む
+                    for row in islice(rows, 29):
+                        writer.writerow(row)
+
+                    print("#### csvファイルに保存しました。 ####")
+
+            except IOError as e:
+                print(f"ファイル書き込みエラー：{e}")
+
         print("#### カーソルを閉じました。 ####")
 
     except psycopg2.Error as e:
